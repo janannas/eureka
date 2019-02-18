df0b71a88d2f6b1b8f1e85d709c9669503b3ed70
Johanna <josjlin@gmail.com>
2019-02-18 11:03:21 +0100

    modal "window" attached to showModal state

---
+++ src/components/GalleryModal.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import styled from 'styled-components';
+
+export function GalleryModal(props) {
+  // The gray background
+  const Backdrop = styled.div`
+    position: fixed;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    background-color: rgba(0,0,0,0.3);
+    padding: 50px;
+  `;
+
+  // The modal "window"
+  const Modal = styled.div`
+    background-color: #fff;
+    border-radius: 5px;
+    max-width: 600px;
+    min-height: 300px;
+    margin: 0 auto;
+    padding: 30px;
+    border: solid 2px red;
+  `;
+  const { src } = props;
+
+  return (
+    <Backdrop>
+      <Modal>
+        <img src={src} alt="" style={{ width: '100%' }} />
+      </Modal>
+    </Backdrop>
+  );
+}
--- src/containers/App.js
+++ src/containers/App.js
@@ -3,11 +3,13 @@ import PropTypes from "prop-types";
 import { connect } from 'react-redux';
 
 import { GalleryImage } from '../components/GalleryImage';
+import { GalleryModal } from '../components/GalleryModal';
 
 import { toggleModal } from '../js/actions/index';
 
 const mapStateToProps = state => {
   return {
+    showModal: state.showModal,
     galleryUrl: state.galleryUrl
   };
 }
@@ -24,11 +26,12 @@ class ConnectedApp extends Component {
   }
 
   render() {
-    const { galleryUrl } = this.props;
+    const { galleryUrl, showModal } = this.props;
     console.log(galleryUrl);
 
     return (
       <>
+
         {
           galleryUrl.map((el, index) =>
             <GalleryImage
@@ -38,12 +41,25 @@ class ConnectedApp extends Component {
             />
           )
         }
+        {
+          showModal
+            ?
+            (
+              <GalleryModal />
+            ) :
+            (
+              null
+            )
+
+        }
+
       </>
     );
   }
 }
 
 ConnectedApp.propTypes = {
+  showModal: PropTypes.bool,
   galleryUrl: PropTypes.array,
   toggleModal: PropTypes.func
 }
